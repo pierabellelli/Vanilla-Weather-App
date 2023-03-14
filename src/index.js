edd3f5b50a2a29034b28ab8595f13dace5b5d6ee
Piera Bellelli <piera.bellelli@gmail.com>
2023-03-14 18:23:11 +0100

    updating weather

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
+function displayCity(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  console.log(response.data.city);
+}
+
 function displayTemperature(response) {
   console.log(response.data.temperature.current);
-  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
 function displayHumidity(response) {
   console.log(response.data.temperature.humidity);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
 }
 function displayWind(response) {
   console.log(response.data.wind.speed);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 function displayPressure(response) {
   console.log(response.data.temperature.pressure);
+  let pressureElement = document.querySelector("#pressure");
+  pressureElement.innerHTML = response.data.temperature.pressure;
 }
 function displayCondition(response) {
   console.log(response.data.condition.description);
+  let conditionElement = document.querySelector("#condition");
+  conditionElement.innerHTML = response.data.condition.description;
 }
 let apiKey = "tf432db404oa6c294f5f02645370860b";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Rome&key=tf432db404oa6c294f5f02645370860b&units=metric";
 console.log(apiUrl);
+axios.get(apiUrl).then(displayCity);
 axios.get(apiUrl).then(displayTemperature);
 axios.get(apiUrl).then(displayHumidity);
 axios.get(apiUrl).then(displayWind);
