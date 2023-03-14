08b9a105344908046eb638b91a82ba47f5f42dcb
Piera Bellelli <piera.bellelli@gmail.com>
2023-03-14 18:53:14 +0100

    updating date time days

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 function displayCity(response) {
+  console.log(response.data);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   console.log(response.data.city);
 }
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
+  return `${day}, ${hours}:${minutes}`;
+}
+function displayDate(response) {
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+}
 function displayTemperature(response) {
   console.log(response.data.temperature.current);
   let temperatureElement = document.querySelector("#temperature");
@@ -34,6 +61,7 @@ let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Rome&key=tf432db404oa6c294f5f02645370860b&units=metric";
 console.log(apiUrl);
 axios.get(apiUrl).then(displayCity);
+axios.get(apiUrl).then(displayDate);
 axios.get(apiUrl).then(displayTemperature);
 axios.get(apiUrl).then(displayHumidity);
 axios.get(apiUrl).then(displayWind);
